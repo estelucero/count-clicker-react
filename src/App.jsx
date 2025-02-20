a65a058a8da01b18604941ca0be2ad14de74bad3
naticabezas <nati.cabezas94@gmail.com>
2025-02-20 18:16:16 -0300

    agrego react-logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import reactLogo from './assets/images/react-logo.png'
 function App() {
   return (
     <div className='App'>
+      <div className='react-logo-contenedor'>
+        <img className='react-logo'
+          src={reactLogo}
+          alt='react-logo'></img>
+      </div>
     </div>
   );
 }
